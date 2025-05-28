246d87b4e1442ea8a99c117aea1e93fd4558eda2
JA+ <601286825@qq.com>
2025-05-28 16:07:53 +0800

    submit code tree

---
+++ docs-demo/basic/tree/Tree.vue
@@ -0,0 +1,54 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import StkTable from '../../StkTable.vue';
+import { StkTableColumn } from '../../../src/StkTable/index';
+
+const columns: StkTableColumn<any>[] = [
+    { type: 'tree-node', title: 'Area', dataIndex: 'area' },
+    { title: 'GDP', dataIndex: 'gdp' },
+    { title: 'Population', dataIndex: 'population' },
+    { title: 'GDP per capita', dataIndex: 'gdpPerCapita' },
+];
+
+const dataSource = ref([
+    {
+        area: 'Asia',
+        gdp: 10000,
+        population: 50000000,
+        gdpPerCapita: 20000,
+        children: [
+            {
+                area: 'China',
+                gdp: 5000,
+                population: 1400000000,
+                gdpPerCapita: 35000,
+                children: [
+                    { area: 'Beijing', gdp: 2000, population: 21000000, gdpPerCapita: 9000 },
+                    { area: 'Shanghai', gdp: 3000, population: 24000000, gdpPerCapita: 12000 },
+                    { area: 'Guangzhou', gdp: 4000, population: 23000000, gdpPerCapita: 11000 },
+                ],
+            },
+            {
+                area: 'Japan',
+                gdp: 4000,
+                population: 126000000,
+                gdpPerCapita: 33000,
+                children: [{ area: 'Tokyo', gdp: 1000, population: 13900000, gdpPerCapita: 7000 }],
+            },
+            {
+                area: 'Korea',
+                gdp: 3000,
+                population: 51000000,
+            },
+        ],
+    },
+    { area: 'Europe', gdp: 8000, population: 740000000 },
+    { area: 'Africa', gdp: 6000, population: 1010000000 },
+    { area: 'North America', gdp: 5000, population: 301000000 },
+    { area: 'South America', gdp: 4000, population: 421000000 },
+    { area: 'Australia', gdp: 3000, population: 251000000 },
+]);
+</script>
+<template>
+    <StkTable :columns="columns" :data-source="dataSource"></StkTable>
+</template>
---
+++ docs-src/main/table/basic/tree.md
@@ -0,0 +1,5 @@
+# 树形
+通过配置 `StkTableColumn['type']` 为 `tree-node` 来指定树形展开按钮的位置。点击后会将数据中的 `children` 字段中的内容，作为子节点进行展示。
+
+## 简单树形
+<demo vue="basic/tree/Tree.vue"></demo>
