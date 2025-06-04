d7a0ca840dbfcba3c895f372d3806e9ad6326cca
JA+ <601286825@qq.com>
2025-06-04 21:32:40 +0800

    submit code

--- docs-src/.vitepress/config.ts
+++ docs-src/.vitepress/config.ts
@@ -93,7 +93,7 @@ export default defineConfig({
                   { text: '行、单元格选中/悬浮', link: '/table/basic/row-cell-mouse-event', },
                   { text: '无头', link: '/table/basic/headless', },
                   { text: '行展开', link: '/table/basic/expand-row', },
-                  { text: '树形', link: '/table/basic/tree', },
+                  { text: '树形(✨NEW)', link: '/table/basic/tree', },
                   { text: '多级表头', link: '/table/basic/multi-header', },
                   { text: '序号列', link: '/table/basic/seq', },
                   { text: '空数据', link: '/table/basic/empty' },
