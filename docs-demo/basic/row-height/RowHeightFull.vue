02c8f152394f6038bb14d41fca87ecd5e321f462
JA+ <601286825@qq.com>
2025-06-04 11:51:37 +0800

    update row-height doc

---
+++ docs-demo/basic/row-height/RowHeightFull.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import StkTable from '../../StkTable.vue';
+import { StkTableColumn } from '../../../src/StkTable/index';
+
+const height = ref(300);
+
+const columns: StkTableColumn<any>[] = [
+    { title: 'Name', dataIndex: 'name' },
+    { title: 'Address', dataIndex: 'address', maxWidth: 200 },
+];
+
+const dataSource = ref([
+    { name: `Jack`, address: `Beijing Forbidden City` },
+    { name: `Tom`, address: `Shanghai` },
+    { name: `Lucy`, address: `Guangzhou` },
+    { name: `Lily`, address: `Shenzhen` },
+]);
+</script>
+<template>
+    <div class="container" :style="{ height: height + 'px' }">
+        <div>
+            <input v-model="height" type="range" min="0" max="600" />
+            <span>height: {{ height }}px</span>
+        </div>
+        <StkTable header-row-height="50" :columns="columns" :data-source="dataSource"></StkTable>
+    </div>
+</template>
+<style scoped>
+.container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
+:deep(.stk-table) {
+    flex: 1;
+}
+:deep(.stk-table .stk-table-main) {
+    height: 100%;
+}
+</style>
--- docs-src/main/table/basic/row-height.md
+++ docs-src/main/table/basic/row-height.md
@@ -1,10 +1,10 @@
 # 行高
 
-* `props.rowHeight` 设置表体行高，默认为 28px。
-* `props.headerRowHeight` 设置表头行高，默认为 28px。
+* `props.rowHeight` 设置表体行高，默认为 `28px`。
+* `props.headerRowHeight` 设置表头行高，默认为 `28px`。
 
 ## 示例
-如下设置了表头行高为 50px，表体行高为 40px。
+如下设置了表头行高为 `50px`，表体行高为 `40px`。
 ```vue
 <template>
     <StkTable row-height="40" header-row-height="50"></StkTable>
@@ -19,3 +19,26 @@
 <demo vue="basic/row-height/RowHeight.vue"></demo>
 
 在开启虚拟列表时，滚动会导致列宽度变化，是没有设置列宽导致的。
+
+
+## 弹性行高
+首先 `<StkTable>` 这个组件的高度必须是足够的。您可以使其处在`flex`、`grid`等弹性布局中，或将高度设为`100%`，这样做的目的是让其父元素决定它的高度，而不是子元素撑开。
+
+其次, 令`.stk-table .stk-table-main` 的 `height` 为 `100%`
+
+如下
+
+```css
+:deep(.stk-table .stk-table-main) {
+    height: 100%;
+}
+```
+
+拖动下方的高度控制器，可以看到行弹性行高。
+<demo vue="basic/row-height/RowHeightFull.vue"></demo>
+在做一些 **行数固定** 的表格会比较有用。
+
+::: info
+当height缩小到一定程度时，可以看到，最小高度依然由表格中的内容决定（min-content）。
+:::
+
