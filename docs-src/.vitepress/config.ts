b2c27a72c0eaa1ef72867f6e0f978f611e24b1cb
ja-plus <601286825@qq.com>
2026-01-28 19:56:18 +0800

    update doc

--- docs-src/.vitepress/config.ts
+++ docs-src/.vitepress/config.ts
@@ -5,44 +5,41 @@ import llmstxt from 'vitepress-plugin-llms';
 import { enConfig } from './src/config/en';
 import { zhConfig } from './src/config/zh';
 
-
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/stk-table-vue',
-  title: "StkTableVue",
-  lastUpdated: true,
-  appearance: 'dark',
-  vite: {
-    plugins: [
-      llmstxt()
-    ]
-  },
-  head: [
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/stk-table-vue/assets/logo.svg' }],
-  ],
-  themeConfig: {
-    logo: '/assets/logo.svg',
-    search: {
-      provider: 'local',
+    base: '/stk-table-vue',
+    title: 'StkTableVue',
+    lastUpdated: true,
+    appearance: 'dark',
+    vite: {
+        plugins: [llmstxt()],
     },
-  },
-  locales: {
-    root: {
-      label: '中文',
-      ...zhConfig
+    head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/stk-table-vue/assets/logo.svg' }]],
+    themeConfig: {
+        logo: '/assets/logo.svg',
+        search: {
+            provider: 'local',
+        },
     },
-    en: {
-      label: 'English',
-      ...enConfig
-    }
-  },
-  markdown: {
-    config(md) {
-      md.use(vitepressDemoPlugin, {
-        demoDir: path.resolve(__dirname, '../../docs-demo'),
-      });
+    locales: {
+        root: {
+            label: '中文',
+            ...zhConfig,
+        },
+        en: {
+            label: 'English',
+            ...enConfig,
+        },
     },
-  }
-})
-
-
+    markdown: {
+        config(md) {
+            md.use(vitepressDemoPlugin, {
+                demoDir: path.resolve(__dirname, '../../docs-demo'),
+                locale: {
+                    zh: 'zh-CN',
+                    'en': 'en-US'
+                },
+            });
+        },
+    },
+});
