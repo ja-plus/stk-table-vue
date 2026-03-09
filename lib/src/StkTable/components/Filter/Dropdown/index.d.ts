11159af3e6517686d26e73cc55d8ea245ec92e40
ja-plus <601286825@qq.com>
2026-03-09 11:16:31 +0800

    0.11.0-beta.4

--- lib/src/StkTable/components/Filter/Dropdown/index.d.ts
+++ lib/src/StkTable/components/Filter/Dropdown/index.d.ts
@@ -1,32 +1,24 @@
-import { FilterOption } from '../types';
-
-export declare function getDropdownIns(onConfirm: (values: FilterOption['value'][]) => void): Promise<import('vue').CreateComponentPublicInstanceWithMixins<Readonly<{}> & Readonly<{
-    onConfirm?: ((options: FilterOption[]) => any) | undefined;
-}>, {
+export declare function getDropdownIns(): Promise<import('vue').CreateComponentPublicInstanceWithMixins<Readonly<{}> & Readonly<{}>, {
     visible: import('vue').Ref<boolean, boolean>;
     show: (pos: {
         x: number;
         y: number;
-    }, opt?: FilterOption[]) => void;
-    clear: () => void;
+    }, opt: import('../types').FilterOption[], onConfirm: (values: import('../types').FilterOption["value"][]) => void) => void;
     hide: () => void;
-}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
-    confirm: (options: FilterOption[]) => void;
-}, import('vue').PublicProps, {}, true, {}, {}, import('vue').GlobalComponents, import('vue').GlobalDirectives, string, {}, any, import('vue').ComponentProvideOptions, {
+    setTheme: (t: "light" | "dark") => void;
+}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, import('vue').PublicProps, {}, true, {}, {}, import('vue').GlobalComponents, import('vue').GlobalDirectives, string, {}, any, import('vue').ComponentProvideOptions, {
     P: {};
     B: {};
     D: {};
     C: {};
     M: {};
     Defaults: {};
-}, Readonly<{}> & Readonly<{
-    onConfirm?: ((options: FilterOption[]) => any) | undefined;
-}>, {
+}, Readonly<{}> & Readonly<{}>, {
     visible: import('vue').Ref<boolean, boolean>;
     show: (pos: {
         x: number;
         y: number;
-    }, opt?: FilterOption[]) => void;
-    clear: () => void;
+    }, opt: import('../types').FilterOption[], onConfirm: (values: import('../types').FilterOption["value"][]) => void) => void;
     hide: () => void;
+    setTheme: (t: "light" | "dark") => void;
 }, {}, {}, {}, {}>>;
--- lib/src/StkTable/components/Filter/Dropdown/index.vue.d.ts
+++ lib/src/StkTable/components/Filter/Dropdown/index.vue.d.ts
@@ -3,17 +3,13 @@ import { FilterOption } from '../types';
 declare function show(pos: {
     x: number;
     y: number;
-}, opt?: FilterOption[]): void;
-declare function clear(): void;
+}, opt: FilterOption[], onConfirm: (values: FilterOption['value'][]) => void): void;
 declare function hide(): void;
+declare function setTheme(t: 'light' | 'dark'): void;
 declare const _default: import('vue').DefineComponent<{}, {
     visible: import('vue').Ref<boolean, boolean>;
     show: typeof show;
-    clear: typeof clear;
     hide: typeof hide;
-}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {
-    confirm: (options: FilterOption[]) => void;
-}, string, import('vue').PublicProps, Readonly<{}> & Readonly<{
-    onConfirm?: ((options: FilterOption[]) => any) | undefined;
-}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {}, any>;
+    setTheme: typeof setTheme;
+}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<{}> & Readonly<{}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {}, any>;
 export default _default;
